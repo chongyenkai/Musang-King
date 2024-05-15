2a39634c35023abf599f2f577c3d5fe87c3b2b03
chongyenkai <85997212+chongyenkai@users.noreply.github.com>
2024-05-15 23:51:32 +0800

    Update table selection page

--- table_management_v.php
+++ table_management_v.php
@@ -1,13 +1,117 @@
 <?php $this->load->view('swe/header_v');?>
 
 <h1>Select Table</h1>
-    <div class="table-layout">
-        <!-- Image with clickable areas -->
-        <img src="restaurant_layout.png" alt="Restaurant Layout" usemap="#tableMap">
-        <!-- Define clickable areas using map -->
-        <map name="tableMap">
-            <area shape="rect" coords="50,50,150,150" href="book_table.php?table=1" alt="Table 1">
-            <area shape="rect" coords="200,50,300,150" href="book_table.php?table=2" alt="Table 2">
-            <!-- Add more areas as needed -->
-        </map>
+<div class="container">
+    <div class="row">
+        <!-- 3x4 Column (spanning 3 columns each) -->
+        <div class="col-12 col-md-2">
+            <div class="row">
+                <div class="col-12 mb-3">
+                    <a href="<?php echo base_url('swe/reservation'); ?>">
+                        <img src="<?php echo base_url('assets/image/swe_image/Rectable.png'); ?>" id="Table 1" class="table-image">
+                    </a>
+                </div>
+                <div class="col-12 mb-3">
+                    <a href="<?php echo base_url('swe/reservation'); ?>">
+                        <img src="<?php echo base_url('assets/image/swe_image/Rectable.png'); ?>" id="Table 2" class="table-image">
+                    </a>                
+                </div>
+                <div class="col-12 mb-3">
+                    <a href="<?php echo base_url('swe/reservation'); ?>">
+                        <img src="<?php echo base_url('assets/image/swe_image/Rectable.png'); ?>" id="Table 3" class="table-image">
+                    </a>     
+                </div>
+                <div class="col-12 mb-3">
+                    <a href="<?php echo base_url('swe/reservation'); ?>">
+                        <img src="<?php echo base_url('assets/image/swe_image/Rectable.png'); ?>" id="Table 4" class="table-image">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-2">
+            <div class="row">
+                <div class="col-12 mb-3">
+                    <a href="<?php echo base_url('swe/reservation'); ?>">
+                        <img src="<?php echo base_url('assets/image/swe_image/Rectable.png'); ?>" id="Table 5" class="table-image">
+                    </a>
+                </div>
+                <div class="col-12 mb-3">
+                    <a href="<?php echo base_url('swe/reservation'); ?>">    
+                        <img src="<?php echo base_url('assets/image/swe_image/Rectable.png'); ?>" id="Table 6" class="table-image">
+                    </a>                
+                </div>
+                <div class="col-12 mb-3">
+                    <a href="<?php echo base_url('swe/reservation'); ?>">
+                        <img src="<?php echo base_url('assets/image/swe_image/Rectable.png'); ?>" id="Table 7" class="table-image">
+                    </a>
+                </div>
+                <div class="col-12 mb-3">
+                    <a href="<?php echo base_url('swe/reservation'); ?>">
+                        <img src="<?php echo base_url('assets/image/swe_image/Rectable.png'); ?>" id="Table 8" class="table-image">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-2">
+            <div class="row">
+                <div class="col-12 mb-3">
+                    <a href="<?php echo base_url('swe/reservation'); ?>">
+                        <img src="<?php echo base_url('assets/image/swe_image/Rectable.png'); ?>" id="Table 9" class="table-image">
+                    </a>
+                </div>
+                <div class="col-12 mb-3">
+                    <a href="<?php echo base_url('swe/reservation'); ?>">    
+                        <img src="<?php echo base_url('assets/image/swe_image/Rectable.png'); ?>" id="Table 10" class="table-image">
+                    </a>
+                </div>
+                <div class="col-12 mb-3">
+                    <a href="<?php echo base_url('swe/reservation'); ?>">
+                        <img src="<?php echo base_url('assets/image/swe_image/Rectable.png'); ?>" id="Table 11" class="table-image">
+                    </a>
+                </div>
+                <div class="col-12 mb-3">
+                    <a href="<?php echo base_url('swe/reservation'); ?>">    
+                        <img src="<?php echo base_url('assets/image/swe_image/Rectable.png'); ?>" id="Table 12" class="table-image">
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <!-- 1x2 Column (spanning 3 columns) -->
+        <div class="col-12 col-md-3">
+            <div class="row">
+                <div class="col-12 mb-3">
+                    <a href="<?php echo base_url('swe/reservation'); ?>">
+                        <img src="<?php echo base_url('assets/image/swe_image/Roundtable.png'); ?>" id="Table 13" class="round-image">
+                    </a>
+                </div>
+                <div class="col-12 mb-3">
+                    <a href="<?php echo base_url('swe/reservation'); ?>">
+                        <img src="<?php echo base_url('assets/image/swe_image/Roundtable.png'); ?>" id="Table 14" class="round-image">
+                    </a>
+                    </div>
+            </div>
+        </div>
+
+        <!-- 1x3 Column (spanning 3 columns) -->
+        <div class="col-12 col-md-3">
+            <div class="row">
+                <div class="col-12 mb-3">
+                    <a href="<?php echo base_url('swe/reservation'); ?>">
+                        <img src="<?php echo base_url('assets/image/swe_image/Squaretable.png'); ?>" id="Table 15" class="square-image">
+                    </a>
+                    </div>
+                <div class="col-12 mb-3">
+                    <a href="<?php echo base_url('swe/reservation'); ?>">
+                        <img src="<?php echo base_url('assets/image/swe_image/Squaretable.png'); ?>" id="Table 16" class="square-image">
+                    </a>
+                    </div>
+                <div class="col-12 mb-3">
+                    <a href="<?php echo base_url('swe/reservation'); ?>">
+                        <img src="<?php echo base_url('assets/image/swe_image/Squaretable.png'); ?>" id="Table 17" class="square-image">
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
