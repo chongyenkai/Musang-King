5b8b06656c050f670a263c2530ad3a7db15b4c6a
chongyenkai <85997212+chongyenkai@users.noreply.github.com>
2024-05-19 22:11:36 +0800

    Update Login controller and model

--- Login.php
+++ Login.php
@@ -4,26 +4,53 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Login extends CI_Controller{
     function __construct() {
 		parent::__construct();
-        $this->load->model('Login_m');
-		
+        $this->load->model('swe/Login_m');
+        $this->load->model('swe/Signup_m');
     }
 
     function index() {
 
-        if($this->Login_m->is_login()){
-            redirect('reservation');
+        if ($this->Login_m->is_login()){
+            redirect('swe/reservation');
         }
 
-        if($this->input->post()){
-            $name = $this->input->post('name');
+        if ($this->input->post()){
+            $email = $this->input->post('email');
             $password = $this->input->post('password');
-        }
 
-        $this->load->view('login_v');
+            //validate the user
+            $user = $this->Login_m->validate_user($email, $password);
+
+            if ($user) {
+                $this->Login_m->create_session($user);
+                redirect('swe/reservation');
+            } else {
+                $this->load->view('swe/login');
+            }
+        } else {
+            $this->load->view('swe/login_v');
+        }
     }
 
     function signup() {
-        $this->load->view('signup_v');
+        if ($this->input->post()) {
+            $name = $this->input->post('name');
+            $email = $this->input->post('email');
+            $password = $this->input->post('password');
+            $confirm_password = $this->input->post('confirm_password');
+            $role = $this->input->post('role');
+
+            //validate the data
+            $registration_result = $this->Signup_m->register($name, $email, $password, $confirm_password, $role);
+
+            if ($registration_result) {
+                redirect('swe/login/index');
+            } else {
+                $this->load->view('swe/signup_v');
+            }
+        } else {
+            $this->load->view('swe/signup_v');
+        }
     }
 }
 ?>
--- Login_m.php
+++ Login_m.php
@@ -8,80 +8,37 @@ class Login_m extends CI_Model{
         $this->load->library('session');
     }
 
-    function validate_data($name,$password){
-        $error_msg = '';
+    function validate_user($email, $password){
+        $sql = "SELECT * FROM `users` WHERE `email` = ? LIMIT 1";
+        $query = $this->db->query($sql, array($email));
 
-        if(empty($name)){
-            $error_msg = 'Name is required';
-        }
-        else{
-            if(!preg_match('/^[a-zA-Z0-9\s]+$/', $name)){
-                $error_msg = "Name can only contain alphabets and numbers.";
+        if($query->num_rows() == 1){
+            $user = $query->row();
+
+            //check if the password match
+            if ($user->password == $password) {
+                return $user;
+            } else {
+                return false;
             }
         }
-
-        if(empty($password)){
-            $error_msg = 'Password is required';
-        }
-
-        return $error_msg;
-    }
-
-    function authenticate_user($name, $password){
-        // $sql = "SELECT * FROM login WHERE name =". $this->db->escape($name). "AND password = ". $this->db->escape($password);
-
-        // $query = $this->db->query($sql);
-
-        // if($query->num_rows() > 0){
-        //     return $query->row_array();
-        // }
-        // else{
-        //     return null;
-        // }
-
-        $this->db->where('name', $name);
-        $query = $this->db->get('users');
-        $user = $query->row_array();
-
-        if($user && password_verify($password, $user['password'])){
-            return $user;
-        }
         else{
-            return null;
+            return false;
         }
     }
 
-    function login_user($name,$password){
-        $login_result = $this->validate_data($name,$password);
-
-        if($login_result !=''){
-            return array(
-                'status' => false,
-                'errors' => $login_result
-            );
-        }
-
-        $user = $this->authenticate_user($name,$password);
-
-        if($user){
-            $this->session->set_userdata('login_status', true);
-            $this->session->set_userdata('user_id',$user['id']);
-            return array('status' => true);
-        }
-        else{
-            return array(
-                'status' => false,
-                'errors' => 'Invalid name or password'
-            );
-        }
+    function create_session($user) {
+        $session_data = array(
+            'id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
+            'role' => $user->role,
+            'logged_in' => TRUE
+        );
+        $this->session->set_userdata($session_data);
     }
 
-    function is_login(){
-        return $this->session->userdata('login_status') == true;
-    }
-
-    function register($data){
-        $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
-        return $this->db->insert('users', $data);
-    }
+    function is_login() {
+		return $this->session->userdata('login_status') === true;
+	}
 }
